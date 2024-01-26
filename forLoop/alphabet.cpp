85908769a851820e5520b12893a8167d1f257765
umit <umitakdag@gmail.com>
2024-01-26 10:29:07 +0200

    forloop

---
+++ forLoop/alphabet.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    
+    for(char i='A'; i<='Z';i++){
+        cout<<i<<" ";
+    }
+
+    return 0;
+}
---
+++ forLoop/sumNumbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num, sum=0;
+    for(int i=1; i<=5;i++){
+        cin>>num;
+        sum=sum+num;
+        
+    }
+
+    cout<<sum;
+
+    return 0;
+}
